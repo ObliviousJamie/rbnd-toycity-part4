173f8f2b4d7670dc318bfe14b2e1c178868dc3f3
Jamie Moreland <jamiemorelanduk@gmail.com>
2016-06-23 11:47:00 +0100

    Add seeding

--- data/seeds.rb
+++ data/seeds.rb
@@ -2,7 +2,14 @@ require 'faker'
 
 # This file contains code that populates the database with
 # fake data for testing purposes
+@@data_path = File.dirname(__FILE__) + "/../data/data.csv"
 
 def db_seed
   # Your code goes here!
+   10.times do
+       brand = Faker::Commerce.department(1)
+       name = Faker::Commerce.product_name
+       price = Faker::Commerce.price
+       Product.create(brand:brand ,name:name ,price:price) 
+   end
 end
