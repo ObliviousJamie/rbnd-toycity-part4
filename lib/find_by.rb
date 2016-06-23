3ba33c6acd006cec14d354707440c30fe162b6fc
Jamie Moreland <jamiemorelanduk@gmail.com>
2016-06-23 14:17:56 +0100

    Pass find_by tests

--- lib/find_by.rb
+++ lib/find_by.rb
@@ -1,6 +1,12 @@
 class Module
   def create_finder_methods(*attributes)
-    # Your code goes here!
-    # Hint: Remember attr_reader and class_eval
+      attributes.each do |attribute|
+        finder = %Q{
+            def find_by_#{attribute}(value)
+                self.all.each{|item| return item if item.#{attribute} == value}
+            end
+        }
+        self.class_eval finder
+      end
   end
 end
--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -3,6 +3,7 @@ require_relative 'errors'
 require 'csv'
 
 class Udacidata
+    attr_reader :name, :brand
     @@data_path = File.dirname(__FILE__) + "/../data/data.csv"
 
     #Methods called on self
@@ -23,12 +24,14 @@ class Udacidata
             CSV.foreach(@@data_path, headers: true) do |row|
                 id = row['id']
                 brand = row['brand']
-                name = row['name']
+                name = row['product']
                 price = row['price']
                 items << self.new(id:id, brand:brand, name:name, price:price)
             end
             return items
         end
+
+        create_finder_methods("brand","name")
 
         def first(number = 1)
             number == 1 ? result = all.first : result = all.take(number)
@@ -48,8 +51,17 @@ class Udacidata
             killed = nil
             data.delete_if{|item| item["id"] == number.to_s && killed = item}
             rewrite(data)
-            return Product.new(id:killed['id'],brand:killed['brand'],name: killed['name'], price:killed['price'])
+            return Product.new(id:killed['id'],brand:killed['brand'],name:killed['name'], price:killed['price'])
         end
+
+
+        #def find_by_brand(brand)
+        #    all.select{|item| item.brand == brand}[0]
+        #end
+
+        #def find_by_name(name)
+        #    all.select{|item| item.name == name}[0]
+        #end
 
         private
         #Reads the latest information from CSV
