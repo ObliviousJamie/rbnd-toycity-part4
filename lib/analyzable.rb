809952c01dcbe763be7ff77efea12d59e0b98490
Jamie Moreland <jamiemorelanduk@gmail.com>
2016-06-23 19:00:03 +0100

    Pass analyzable tests

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -1,3 +1,47 @@
 module Analyzable
-  # Your code goes here!
+    # Your code goes here!
+    def average_price(array)
+        (array.inject(0){|sum,n| sum + (n.price).to_f}/array.size.to_f).round(2)
+    end
+
+    def print_report(products)
+        report = ""
+        brand = count_by_brand products
+        name = count_by_name products
+        
+        report << "Inventory by brand:"
+
+        brand.each do |key, value|
+            report << "  - #{key}: #{value}"
+        end
+        
+        report << "Inventory by name:"
+
+        name.each do |key, value|
+            report << "  - #{key}: #{value}"
+        end
+        return report
+
+    end
+
+    def count_by_name(products)
+        make_hash(products,"name")
+    end
+
+    def count_by_brand(products)
+        make_hash(products,"brand")
+    end
+
+    private
+
+    def make_hash(products, key)
+        hash = Hash.new(0)
+        products.each do |item|
+            converted_key = item.send key.to_sym
+            hash[converted_key] += 1
+            return hash
+        end
+    end
+
+
 end
